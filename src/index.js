e8b876b3b509ee0eb9eb418cd3004bc5c6150699
YanHarnitski <yanharnitski@gmail.com>
2023-08-28 11:03:14 +0200

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let result = "";
+    const zeroToNineteen = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const hundred = Math.floor(number / 100);
+    number = number % 100;
+    const ten = Math.floor(number / 10);
+    const one = number % 10;
+
+    if (number === 0) {
+        result = "zero";
+    }
+    if (hundred) {
+        result = zeroToNineteen[hundred] + " hundred";
+    }
+    if (ten > 0 || one > 0) {
+        if (!(result === "")) {
+            result += " ";
+        }
+
+        if (ten < 2) {
+            result = result + zeroToNineteen[ten * 10 + one];
+        } else {
+            result += dozens[ten];
+            if (one > 0) {
+                result = result + " " + zeroToNineteen[one];
+            }
+        }
+    }
+    return result;
+};
